7465121584a922bafff76a2f285d629cc8924406
Matt Valentine-House <matt@eightbitraptor.com>
2014-11-10 21:08:36 +0000

    WIP: Mysql implementation

---
+++ examples/mysql-project/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to postgres-project
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ examples/mysql-project/test/mysql_project/core_test.clj
@@ -0,0 +1,3 @@
+(ns postgres-project.core-test
+  (:require [clojure.test :refer :all]
+            [postgres-project.core :refer :all]))
