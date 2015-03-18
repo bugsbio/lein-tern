3e0a80d5013264d264e9ac439f26beb2959b4793
Richard Tyler <richard@bugsbio.org>
2015-03-18 16:09:08 +0000

    update test :dog2:

--- test/tern/migrate_test.clj
+++ test/tern/migrate_test.clj
@@ -2,5 +2,5 @@
   (:require [tern.migrate :refer :all]
             [expectations :refer :all]))
 
-(expect "20141123233648"
+(expect "20150318120533"
         (version {:migration-dir "examples/postgres-project/migrations"}))
